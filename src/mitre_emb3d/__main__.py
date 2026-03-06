abc8171355517e5516bfff608b3a46b28e02f196
Sachdeva, Kapil <ksachdeva17@gmail.com>
2026-03-06 16:23:35 +0000

    feat: make heatmap subcommand / introduce init command / remove creation of file from update command

--- src/mitre_emb3d/__main__.py
+++ src/mitre_emb3d/__main__.py
@@ -21,7 +21,7 @@ from mitre_emb3d._graph import (
     write_graphml,
 )
 from mitre_emb3d._locations import cache_directory
-from mitre_emb3d._models import Emb3dCategory, ObjectType, ThreatHeatMap, ThreatResolution, ThreatState
+from mitre_emb3d._models import Emb3dCategory, ThreatHeatMap, ThreatResolution
 from mitre_emb3d._models import StixBundle as ST
 from mitre_emb3d._types import CmdState
 
@@ -37,6 +37,8 @@ LOG_LEVELS = {
 
 
 cli_app = Typer(name=f"MITRE EMB3D Client [{__version__}]")
+heatmap_app = Typer(name="heatmap", help="Commands related to heatmap generation and updates")
+cli_app.add_typer(heatmap_app)
 
 
 @cli_app.callback()
@@ -186,7 +188,29 @@ def serialize_graph(
     write_graphml(G, output)
 
 
-@cli_app.command()
+@heatmap_app.command(name="init")
+def init_heatmap(
+    ctx: typer.Context,
+    name: str,
+    description: str,
+    output_dir: Annotated[Path, typer.Option(help="Path to the directory that would contain the heatmap")],
+) -> None:
+    """Initialize a heatmap JSON file with all threats set to NOT_INVESTIGATED."""
+    state = cast(CmdState, ctx.obj)
+    G = state.graph
+
+    heatmap = make_default_heatmap(
+        G,
+        name=name,
+        description=description,
+    )
+
+    output_dir.mkdir(parents=True, exist_ok=True)
+    heatmap_file = output_dir / "mitr-emb3d-heatmap.json"
+    heatmap_file.write_text(heatmap.model_dump_json(indent=2))
+
+
+@heatmap_app.command(name="update")
 def update_heatmap(
     ctx: typer.Context,
     category: Annotated[Emb3dCategory, typer.Argument(help="Category to update heatmap for")],
@@ -195,20 +219,22 @@ def update_heatmap(
         typer.Argument(help="Threat ID to update (e.g. TID-123)"),
     ],
     threat_resolution: Annotated[ThreatResolution, typer.Argument()],
-    heatmap_file: Annotated[Path, typer.Argument(help="Path to the heatmap JSON file")] = Path(
-        "mitr-emb3d-heatmap.json"
-    ),
+    heatmap_file: Annotated[
+        Path,
+        typer.Option(
+            help="Path to the heatmap JSON file",
+            file_okay=True,
+            dir_okay=False,
+            exists=True,
+        ),
+    ],
 ) -> None:
     """Update the heatmap JSON file with the latest threat states from the graph."""
 
     state = cast(CmdState, ctx.obj)
     G = state.graph
 
-    # check if the file exists, if not create an empty heatmap
-    if heatmap_file.exists():
-        heatmap_data = ThreatHeatMap.model_validate_json(heatmap_file.read_text())
-    else:
-        heatmap_data = make_default_heatmap(G, name="No name", description="No description")
+    heatmap_data = ThreatHeatMap.model_validate_json(heatmap_file.read_text())
 
     # check for this category the threat_id exists in the graph
     vulnerabilities = get_vulnerabilities_by_category(G, category)
