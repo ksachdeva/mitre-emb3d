8d857ca11040c49e1e8f3d562fff552a94b0c25c
Sachdeva, Kapil <ksachdeva17@gmail.com>
2026-03-07 03:05:40 +0000

    feat: in ThreatEntry widget, add the name as the tooltip

--- src/mitre_emb3d/tui/widgets/_threat_entry.py
+++ src/mitre_emb3d/tui/widgets/_threat_entry.py
@@ -1,5 +1,6 @@
 from textual.widgets import Static
 
+from mitre_emb3d._graph import get_threat_from_id
 from mitre_emb3d._models import ThreatState
 
 from ._resolution import RESOLUTION_CSS, RESOLUTION_SHORT
@@ -15,6 +16,10 @@ class ThreatEntry(Static):
         self.threat_id = threat_state.threat_id
         self._threat_state = threat_state
         self.add_class(RESOLUTION_CSS[threat_state.resolution])
+
+    def on_mount(self) -> None:
+        threat = get_threat_from_id(self.app.graph, self.threat_id)  # type: ignore
+        self.tooltip = threat.name
 
     def on_click(self) -> None:
         self.app.push_screen(ThreatModal(self._threat_state))
